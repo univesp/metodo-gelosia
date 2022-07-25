d4412069748fc8aead2d404ab5a182ae1eec5684
GalloGabriel <gabriel.gallo2242@gmail.com>
2022-07-25 18:04:08 -0300

    iniciando parte interativa

--- javascripts/events.js
+++ javascripts/events.js
@@ -20,3 +20,58 @@ $(document).ready(function(){
   // Seu código abaixo
 
 })
+
+function escolherNumeros(){
+  let numero1 = document.querySelector('#num1').value;
+  let numero2 = document.querySelector('#num2').value; 
+  let numerosMultiplicados = numero1 * numero2;
+  let metodoGelosiaContainer = document.querySelector('.gelosia-container')
+
+  let inputArea = document.querySelector('.inputs-box');
+
+  let metodoContainer = document.querySelector('.gelosia-container');
+
+  let arrayNums = [];
+
+  arrayNums.push(numero1.charAt(0), numero1.charAt(1), numero1.charAt(2), numero2.charAt(0), numero2.charAt(1), numero2.charAt(2));
+
+  let arrayFiltered = arrayNums.filter(Boolean);
+  
+  arrayFiltered.forEach(nums => {
+    let boxMetodoGelosia = criaElementoBox();
+    metodoGelosiaContainer.appendChild(boxMetodoGelosia);
+  })
+  
+
+  if(numero1 === "" || numero2 === ""){
+    alert('Por favor insira 2 números.')
+  }else{
+    console.log(numero1.length)
+
+    inputArea.style.display = 'none';
+  
+    metodoContainer.style.display = 'flex';
+  }
+ 
+}
+
+let criaElementoBox = function(){
+    let box = document.createElement("div");
+    box.classList.add('gelosia-box');
+    box.setAttribute('id', 'teste');
+
+    let spanFirstNumber = document.createElement("span");
+    spanFirstNumber.classList.add('gelosia-span1');
+
+    let spanSecondNumber = document.createElement("span");
+    spanSecondNumber.classList.add('gelosia-span2');
+
+    let risquinho = document.createElement("span");
+    risquinho.classList.add('division-box');
+
+    box.appendChild(spanFirstNumber);
+    box.appendChild(spanSecondNumber);
+    box.appendChild(risquinho);
+
+    return box;
+}

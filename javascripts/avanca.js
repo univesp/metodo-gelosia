9eb3e12ba6cbeea0512e3f7724f150a046657298
Alberto Ortiz Mendes <35493648+AlbertoOrtizMendes@users.noreply.github.com>
2022-10-19 11:16:36 -0300

    Add files via upload

---
+++ javascripts/avanca.js
@@ -0,0 +1,114 @@
+let avanca = function(){
+    let input = document.querySelector('.input-num-control')
+    let valor = input.value
+    //Primeiro ato
+    if(objeto.verificaFimDo1Ato()){
+      let letra = ''
+      let primeiroTriangulo = document.querySelector('#span_' + objeto.ids[2*(objeto.cena-1)]) 
+      let segundoTriangulo = document.querySelector('#span_' + objeto.ids[2*(objeto.cena-1)+1])
+      if(valor < 10){
+        letra = '0' + valor;
+      }
+      else{
+        letra = valor.toString();
+      }
+      if(objeto.multiplicacaoConformeCena() != valor){
+        alert('Valor está errado')
+        return 0
+      }
+      zeraInput()
+      primeiroTriangulo.innerHTML = letra[0]
+      segundoTriangulo.innerHTML = letra[1]
+      objeto.cena++;
+      if(objeto.verificaFimDo1Ato()){
+        objeto.atualizaSpans();
+      }
+      else{
+        let cenaDoSegundoAto = objeto.cena - objeto.getNumeroDeCelulas()
+        objeto.descoloreTodasAsDiagonais()
+        objeto.coloreDiagonal(cenaDoSegundoAto)
+        limpaAreaControls()
+        objeto.preencheSpansDiagonal(cenaDoSegundoAto)
+      }
+      removeAllMarcados()
+      if(objeto.verificaFimDo1Ato()){
+        objeto.atualizaResultadosCimaEDireita()
+      }
+    }
+    //Segundo ato
+    else{
+      revelaAvanca()
+      revelaInput()
+      if(objeto.getNumeroDeDiagonais() >= objeto.getCenaDoSegundoAto()){
+        let time = "0"
+        if(objeto.somaDiagonal(objeto.getCenaDoSegundoAto()) != valor){
+          alert('Valor está errado')
+          return 0
+        }
+        //dinâmica das laterais
+        if(objeto.getCenaDoSegundoAto() <= objeto.nColunas()){
+          let elemento = document.getElementById('lateral_' + objeto.getCenaDoSegundoAto())
+          elemento.innerHTML = valor
+          if(valor >= 10){
+            escondeAvanca()
+            escondeInput()
+            dinamicaLateral(valor)
+            time = "6000"
+          }
+          else{
+            revelaAvanca()
+            revelaInput()
+            elemento.innerHTML = valor
+            time = "0"
+          }
+  
+        }
+        //dinâmica das colunas na parte de baixo
+        else{
+          let id = objeto.getCenaDoSegundoAto() - objeto.nColunas()
+          let elemento = document.getElementById('coluna_baixo_' + id)
+          elemento.innerHTML = valor
+          if(valor >= 10){
+            escondeAvanca()
+            escondeInput()
+            dinamicaColunaBaixo(valor)
+            time = "4500"
+          }
+          else{
+            revelaAvanca()
+            revelaInput()
+            elemento.innerHTML = valor
+            time = "0"
+          }
+        }
+        setTimeout(() => {
+          objeto.descoloreTodasAsDiagonais()
+          revelaAvanca()
+          revelaInput()
+        }, time)
+        objeto.cena++
+        
+        if(objeto.getNumeroDeDiagonais() >= objeto.getCenaDoSegundoAto()){
+          limpaAreaControls()
+          setTimeout(() => {
+            objeto.preencheSpansDiagonal(objeto.getCenaDoSegundoAto())
+            objeto.coloreDiagonal(objeto.getCenaDoSegundoAto())
+            zeraInput()
+            revelaAvanca()
+            revelaInput()
+          }, time)
+        }
+        else{
+          zeraInput()
+          limpaAreaControls()
+          objeto.descoloreTodasAsDiagonais()
+          objeto.escondeTodosOsTriangulosDiagonais()
+          objeto.voltaDiagonaisCorPreta()
+          objeto.finaliza()
+        }
+      }
+      else{
+        console.log('acabou')
+      }
+    }
+  }
---
+++ javascripts/objeto.js
@@ -0,0 +1,201 @@
+let objeto = {
+    cena: 0,
+    numeroVertical: 0,
+    numeroHorizontal: 0,
+    ids: [],
+    idsDiagonais: [
+      ['span_linha_1_coluna_1_cima'],
+      ['span_linha_1_coluna_2_cima' ,'span_linha_1_coluna_1_baixo','span_linha_2_coluna_1_cima' ],
+      ['span_linha_1_coluna_3_cima' ,'span_linha_1_coluna_2_baixo','span_linha_2_coluna_2_cima' ,'span_linha_2_coluna_1_baixo','span_linha_3_coluna_1_cima' ],
+      ['span_linha_1_coluna_3_baixo','span_linha_2_coluna_3_cima' ,'span_linha_2_coluna_2_baixo','span_linha_3_coluna_2_cima' ,'span_linha_3_coluna_1_baixo'],
+      ['span_linha_2_coluna_3_baixo','span_linha_3_coluna_3_cima' ,'span_linha_3_coluna_2_baixo'],
+      ['span_linha_3_coluna_3_baixo'],  
+    ],
+    idsDiagonaisCimaBaixo:[
+      ['linha_1_coluna_1_cimaFundo'],
+      ['linha_1_coluna_2_cimaFundo','linha_1_coluna_1_baixoFundo','linha_2_coluna_1_cimaFundo'],
+      ['linha_1_coluna_3_cimaFundo','linha_1_coluna_2_baixoFundo','linha_2_coluna_2_cimaFundo','linha_2_coluna_1_baixoFundo','linha_3_coluna_1_cimaFundo'],
+      ['linha_1_coluna_3_baixoFundo','linha_2_coluna_3_cimaFundo','linha_2_coluna_2_baixoFundo','linha_3_coluna_2_cimaFundo','linha_3_coluna_1_baixoFundo','linha_3_coluna_1_baixoFundo'],
+      ['linha_2_coluna_3_baixoFundo','linha_3_coluna_3_cimaFundo','linha_3_coluna_2_baixoFundo'],
+      ['linha_3_coluna_3_baixoFundo']
+    ],
+    somaDiagonal : function(n){
+      let soma = 0
+      for (let i = 0; i < this.idsDiagonais[n-1].length; i++) {
+        let id = this.idsDiagonais[n-1][i];
+        let elemento = document.getElementById(id)
+        if(document.body.contains(elemento)){
+          soma += parseInt(elemento.innerHTML)
+        }
+      }
+      return(soma)
+    },
+    coloreDiagonal : function(n){
+      this.voltaDiagonaisCorPreta()
+      this.escondeTodosOsTriangulosDiagonais()
+      for (let i = 0; i < this.idsDiagonaisCimaBaixo[n-1].length; i++) {
+        let id = this.idsDiagonaisCimaBaixo[n-1][i];
+        let elemento = document.getElementById(id)
+        if(document.body.contains(elemento)){
+          elemento.style.display = 'flex'
+        }
+      }
+      for (let i = 0; i < this.idsDiagonais[n-1].length; i++) {
+        let id = this.idsDiagonais[n-1][i];
+        let elemento = document.getElementById(id)      
+        if(document.body.contains(elemento)){
+          elemento.style.backgroundColor = '#FF9600'
+          elemento.style.color = 'white'
+        }
+      }
+    },
+    voltaDiagonaisCorPreta : function(){
+      for (let i = 0; i < 6; i++) {
+        let diagonal = this.idsDiagonais[i];
+        for (let j = 0; j < diagonal.length; j++) {
+          let id = diagonal[j];
+          let elemento = document.getElementById(id)
+          if(document.body.contains(elemento)){
+            elemento.style.color = 'black'
+          }
+        }
+      }
+    },
+    descoloreTodasAsDiagonais : function(){
+      for (let i = 0; i < 6; i++) {
+        let diagonal = this.idsDiagonais[i];
+        for (let j = 0; j < diagonal.length; j++) {
+          let id = diagonal[j];
+          let elemento = document.getElementById(id)
+          if(document.body.contains(elemento)){
+            elemento.style.backgroundColor = 'white'
+          }
+        }
+      }
+    },
+    escondeTodosOsTriangulosDiagonais: function(){
+      for (let i = 0; i < 6; i++) {
+        let diagonal = this.idsDiagonaisCimaBaixo[i];
+        for (let j = 0; j < diagonal.length; j++) {
+          let id = diagonal[j];
+          let elemento = document.getElementById(id)
+          if(document.body.contains(elemento)){
+            elemento.style.display = 'none'
+          }
+        }
+      }    
+    },
+    preencheSpansDiagonal : function(n){
+      let areaControls = document.querySelector('.area-controls')
+      for (let i = 0; i < this.idsDiagonais[n-1].length; i++) {
+        let id = this.idsDiagonais[n-1][i];
+        let elemento = document.getElementById(id)
+        if(document.body.contains(elemento)){
+          let valor = elemento.innerHTML;
+          let span = document.createElement('span')
+          span.innerHTML = valor
+          span.classList.add('controleNum')
+          areaControls.appendChild(span)
+          let simboloMais = document.createElement('span')
+          simboloMais.classList.add('controleMult')
+          simboloMais.innerHTML = '+'
+          areaControls.appendChild(simboloMais)
+        }
+      }
+      areaControls.removeChild(areaControls.lastElementChild);
+    },
+    getDigitoNumero : function(vertical, digito){
+      let number = ''
+      if(vertical)
+        number = this.numeroHorizontal.toString()
+      else
+        number = this.numeroVertical.toString()
+      return(number[digito-1])
+    },
+    multiplicacaoConformeCena : function(){
+      let linha = this.nQualLinhaEstou()
+      let coluna = this.nQualColunaEstou()
+      let valor1 = parseInt(this.numeroHorizontal.toString()[linha-1])
+      let valor2 = parseInt(this.numeroVertical.toString()[coluna-1])
+      return(valor1*valor2)
+    },
+    atualizaResultadosCimaEDireita: function(){
+        let resultadoDireita = document.querySelector('#' + 'resultado_direita_' + this.nQualLinhaEstou())
+        let resultadoCima    = document.querySelector('#' + 'resultado_cima_' + this.nQualColunaEstou())
+        resultadoDireita.classList.add('marcado')
+        resultadoCima.classList.add('marcado')
+    },
+    atualizaSpans: function(){
+      let qualLinhaEstou  = this.nQualLinhaEstou()
+      let qualColunaEstou = this.nQualColunaEstou()
+      atualizaSpan1(parseInt(this.numeroVertical.toString()[qualColunaEstou-1]))
+      atualizaSpan2(parseInt(this.numeroHorizontal.toString()[qualLinhaEstou-1]))
+    },
+    verificaFimDo1Ato: function(){
+      return ((this.cena-1) < this.getNumeroDeCelulas())
+    },
+    getNumeroDeCelulas: function(){
+      let digitoHorizontal = this.numeroVertical.toString().length
+      let digitoVertical = this.numeroHorizontal.toString().length
+      return(digitoHorizontal*digitoVertical)
+    },
+    getNumeroDeDiagonais: function(){
+      let linha  = this.numeroVertical.toString().length
+      let coluna = this.numeroHorizontal.toString().length
+      let nDiagonais = 0
+      nDiagonais = linha + coluna
+      return(nDiagonais)
+    },
+    nLinhas: function(){
+      return(this.numeroVertical.toString().length)
+    },
+    nColunas: function(){
+      return(this.numeroHorizontal.toString().length)
+    },
+    nQualColunaEstou: function(){
+      let coluna = 0
+      if(this.cena % this.numeroVertical.toString().length == 0){
+        coluna = this.numeroVertical.toString().length
+      }
+      else{
+        coluna = this.cena % this.numeroVertical.toString().length
+      } 
+      return(coluna)
+    },
+    nQualLinhaEstou: function(){
+      let linha = 0
+      if(this.cena % this.numeroVertical.toString().length == 0){
+        linha = this.cena / this.numeroVertical.toString().length
+      }
+      else{
+        linha = Math.ceil(this.cena/this.numeroVertical.toString().length)
+      }
+      return(linha) 
+    },
+    getCenaDoSegundoAto : function(){
+      return(this.cena - this.getNumeroDeCelulas())
+    },
+    finaliza : function(){
+      escondeAvanca()
+      escondeInput()
+      let linhas = this.nLinhas();
+      let colunas = this.nColunas();
+      let laterais = []
+      let baixos = []
+      let delay = 500
+      for (let i = 1; i <= colunas; i++) {
+        laterais.push(document.querySelector('#lateral_' + i))
+      }
+      for (let i = 1; i <= linhas; i++) {
+        baixos.push(document.querySelector('#coluna_baixo_'+ i))
+      }
+      laterais.forEach(lateral => {
+        marcadoTimeOut(lateral, delay.toString())
+        delay += 500
+      });
+      baixos.forEach(baixo => {
+        marcadoTimeOut(baixo, delay.toString())
+        delay += 500
+      });
+    }
+  }
